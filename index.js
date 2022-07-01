32925ce0c848b60a088a093ad87dc5cc033ff1c5
Renann Prado <renann.prado@emma-sleep.com>
2022-07-01 15:56:02 +0200

    Implement conditional usage of step output instead of env var

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function sanitizeGithubWorkflowName(name) {
   return nameTruncated
 }
 
-function exportCredentials(params){
+function exportCredentials(params, useStepOutput){
   // Configure the AWS CLI and AWS SDKs using environment variables and set them as secrets.
   // Setting the credentials as secrets masks them in Github Actions logs
   const {accessKeyId, secretAccessKey, sessionToken} = params;
@@ -147,18 +147,31 @@ function exportCredentials(params){
   // AWS_ACCESS_KEY_ID:
   // Specifies an AWS access key associated with an IAM user or role
   core.setSecret(accessKeyId);
-  core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
+
+  if (useStepOutput) {
+    core.setOutput('aws-access-key-id', accessKeyId);
+  } else {
+    core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
+  }
 
   // AWS_SECRET_ACCESS_KEY:
   // Specifies the secret key associated with the access key. This is essentially the "password" for the access key.
   core.setSecret(secretAccessKey);
-  core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
+  if (useStepOutput) {
+    core.setOutput('aws-secret-access-key', secretAccessKey);
+  } else {
+    core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
+  }
 
   // AWS_SESSION_TOKEN:
   // Specifies the session token value that is required if you are using temporary security credentials.
   if (sessionToken) {
     core.setSecret(sessionToken);
-    core.exportVariable('AWS_SESSION_TOKEN', sessionToken);
+    if (useStepOutput) {
+      core.setOutput('aws-session-token', sessionToken);
+    } else {
+      core.exportVariable('AWS_SESSION_TOKEN', sessionToken);
+    }
   } else if (process.env.AWS_SESSION_TOKEN) {
     // clear session token from previous credentials action
     core.exportVariable('AWS_SESSION_TOKEN', '');
@@ -172,9 +185,9 @@ function exportRegion(region) {
   core.exportVariable('AWS_REGION', region);
 }
 
-async function exportAccountId(maskAccountId, region) {
+async function exportAccountId(maskAccountId, region, credentials) {
   // Get the AWS account ID
-  const sts = getStsClient(region);
+  const sts = getStsClient(region, credentials);
   const identity = await sts.getCallerIdentity().promise();
   const accountId = identity.Account;
   if (!maskAccountId || maskAccountId.toLowerCase() == 'true') {
@@ -228,11 +241,12 @@ async function validateCredentials(expectedAccessKeyId) {
   }
 }
 
-function getStsClient(region) {
+function getStsClient(region, credentials) {
   return new aws.STS({
     region,
     stsRegionalEndpoints: 'regional',
-    customUserAgent: USER_AGENT
+    customUserAgent: USER_AGENT,
+    ...credentials
   });
 }
 
@@ -274,6 +288,7 @@ async function run() {
     const roleSkipSessionTaggingInput = core.getInput('role-skip-session-tagging', { required: false })|| 'false';
     const roleSkipSessionTagging = roleSkipSessionTaggingInput.toLowerCase() === 'true';
     const webIdentityTokenFile = core.getInput('web-identity-token-file', { required: false });
+    const useStepOutput = (core.getInput('use-step-output', { required: false }) || 'false') === 'true';
 
     if (!region.match(REGION_REGEX)) {
       throw new Error(`Region is not valid: ${region}`);
@@ -301,7 +316,7 @@ async function run() {
         throw new Error("'aws-secret-access-key' must be provided if 'aws-access-key-id' is provided");
       }
 
-      exportCredentials({accessKeyId, secretAccessKey, sessionToken});
+      exportCredentials({accessKeyId, secretAccessKey, sessionToken}, useStepOutput);
     }
     
     // Attempt to load credentials from the GitHub OIDC provider.
@@ -338,7 +353,7 @@ async function run() {
             webIdentityTokenFile,
             webIdentityToken
           }) }, true);
-      exportCredentials(roleCredentials);
+      exportCredentials(roleCredentials, useStepOutput);
       // We need to validate the credentials in 2 of our use-cases
       // First: self-hosted runners. If the GITHUB_ACTIONS environment variable
       //  is set to `true` then we are NOT in a self-hosted runner.
@@ -346,7 +361,7 @@ async function run() {
       if (!process.env.GITHUB_ACTIONS || accessKeyId) {
         await validateCredentials(roleCredentials.accessKeyId);
       }
-      await exportAccountId(maskAccountId, region);
+      await exportAccountId(maskAccountId, region, roleCredentials);
     }
   }
   catch (error) {
